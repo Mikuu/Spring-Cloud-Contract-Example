e97425623aae93670bea5007a6e9d3640c58adcb
mikuu <biao.fu@thoughtworks.com>
2020-08-25 21:52:16 +0800

    ^_^

---
+++ example-consumer-miku/src/test/java/ariman/scc/consumer/miku/MikuIntegrationTest.java
@@ -0,0 +1,26 @@
+package ariman.scc.consumer.miku;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = "example:example-provider:+:stubs:8080")
+public class MikuIntegrationTest {
+
+    @Test
+    public void testWithRestTemplate() throws Exception {
+        Information information = new ProviderService().getInformation();
+
+        Assertions.assertThat(information.getName()).isEqualTo("Hatsune Miku");
+        Assertions.assertThat(information.getNationality()).isEqualTo("Japan");
+
+    }
+}
---
+++ example-consumer-nanoha/src/test/java/ariman/scc/consumer/nanoha/NanohaIntegrationTest.java
@@ -0,0 +1,27 @@
+package ariman.scc.consumer.nanoha;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = "example:example-provider:+:stubs:8080")
+public class NanohaIntegrationTest {
+
+    @Test
+    public void testWithRestTemplate() throws Exception {
+        Information information = new ProviderService().getInformation();
+
+        Assertions.assertThat(information.getName()).isEqualTo("Takamachi Nanoha");
+        Assertions.assertThat(information.getSalary()).isEqualTo(80000);
+        Assertions.assertThat(information.getNationality()).isEqualTo("Japan");
+
+    }
+}
